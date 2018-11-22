f74e57ac86c936033458dc23e03f6453647db903
mifurm <michal.furmankiewicz@hotmail.com>
2018-11-22 21:57:21 +0100

    script to enable ServiceMap agent on all VM's

---
+++ AddServiceMapAgents2AllVMs.ps1
@@ -0,0 +1,24 @@
+$version = "9.4"
+$ExtPublisher = "Microsoft.Azure.Monitoring.DependencyAgent"
+$OsExtensionMap = @{ "Windows" = "DependencyAgentWindows"; "Linux" = "DependencyAgentLinux" }
+
+$rgs = Get-AzureRmResourceGroup
+foreach ($rmgroup in $rgs)
+{
+$group=$rmgroup.ResourceGroupName 
+Write-Output $group
+Get-AzureRmVM -ResourceGroupName $group |
+ForEach-Object {
+    ""
+    $name = $_.Name
+    $os = $_.StorageProfile.OsDisk.OsType
+    $location = $_.Location
+    $vmRmGroup = $_.ResourceGroupName
+    "${name}: ${os} (${location})"
+    Date -Format o
+    $ext = $OsExtensionMap.($os.ToString())
+    $result = Set-AzureRmVMExtension -ResourceGroupName $vmRmGroup -VMName $name -Location $location `
+    -Publisher $ExtPublisher -ExtensionType $ext -Name "DependencyAgent" -TypeHandlerVersion $version
+    $result.IsSuccessStatusCode
+}
+}
